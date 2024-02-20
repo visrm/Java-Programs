26fb13351a020ac3a4a1af7041d3be63c14c425e
V <77574839+visrm@users.noreply.github.com>
2024-02-20 12:39:09 +0530

    Update FirstNOddorEven.java
    spacing changes & compound assignment operator used(+=)

--- FirstNOddorEven.java
+++ FirstNOddorEven.java
@@ -3,16 +3,16 @@ class FirstNOddorEven
 {
 	public static void main(String args[])
 	{
-		Scanner obj=new Scanner(System.in);
+		Scanner obj= new Scanner(System.in);
 		System.out.println("Enter a limit:");
-		int n=obj.nextInt();
-		int i=1,evensum=0,oddsum=0;
-		while(i<=n)
+		int n= obj.nextInt();
+		int i= 1,evensum= 0,oddsum= 0;
+		while(i<= n)
 		{
-			if(i%2==0)
-				evensum=evensum+i;
+			if(i%2== 0)
+				evensum += i;
 			else
-				oddsum=oddsum+i;
+				oddsum += i;
 			i++;
 		}
 		System.out.println("The sum of even numbers is "+evensum);
