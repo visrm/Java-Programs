748dc19c84b1d52e3d706b0161608536f694462c
V <77574839+visrm@users.noreply.github.com>
2024-02-22 11:50:29 +0530

    Update Grade.java
    New program written modifying old version with separate functions and while loop added

--- Grade.java
+++ Grade.java
@@ -1,41 +1,38 @@
-import java.io.*;
 import java.util.*;
 class Grade
 {
-	public static void main(String arg[])
-	{	
-		Scanner obj=new Scanner(System.in);
-		System.out.println("Enter mark 1:");
-		int m1=obj.nextInt();
-		System.out.println("Enter mark 2:");
-		int m2=obj.nextInt();
-		System.out.println("Enter mark 3:");
-		int m3=obj.nextInt();
-		float t=(float)m1+m2+m3;
-		float pt=(t/300)*100;
-		if(pt>=90.0)
+	static void compute(float t, int sub, int max) {
+	   int total= sub*max;
+   	float pt=(t/total)*100;
+		if(pt>=90.0) System.out.println("You secured A Grade");
+		else if(pt>=80.0) System.out.println("You secured B Grade");
+		else if(pt>=70.0) System.out.println("You secured C Grade");
+		else if(pt>=60.0) System.out.println("You secured D Grade");
+		else if(pt>=50.0) System.out.println("You secured E Grade");
+		else System.out.println("You've Failed");	
+   }
+	public static void main(String arg[]) {	
+	    int[] m = new int[10];
+	    int i= 0;
+	    float t= 0;
+		Scanner obj= new Scanner(System.in);
+		System.out.println("Enter Number of subjects: ");
+		int sub = obj.nextInt();
+		System.out.println("Enter Maximum attainable marks: ");
+		int max = obj.nextInt();
+		
+		while(i<sub)
 		{
-			System.out.println("You secured Grade A");
+			System.out.println("Enter mark"+(i+1)+": ");
+		    m[i] = obj.nextInt();
+		    if(m[i]>max) 
+		    { 
+		      System.out.println("Marks exceeds maximum limit.");
+		      break;
+		    }
+		   else t = (float)t +m[i];
+			i++;
 		}
-		else if(pt>=80.0)
-		{
-			System.out.println("You secured Grade B");
-		}
-		else if(pt>=70.0)
-		{
-			System.out.println("You secured Grade C");
-		}
-		else if(pt>=60.0)
-		{
-			System.out.println("You secured Grade D");
-		}
-		else if(pt>=50.0)
-		{
-			System.out.println("You secured Grade E");
-		}
-		else
-		{
-			System.out.println("Failed");
-		}	
+		if(i==sub) compute(t, sub, max);
 	}
 }
