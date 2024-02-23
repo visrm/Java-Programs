909d174061295f5fe64458fe9b6e078032dfc1aa
V <77574839+visrm@users.noreply.github.com>
2024-02-23 15:50:44 +0530

    🎨 LargestOfThree.java

--- LargestOfThree.java
+++ LargestOfThree.java
@@ -1,34 +1,28 @@
-import java.io.*;
 import java.util.*;
 class LargestOfThree
 {
-	public static void main(String arg[])
-	{	
-		Scanner obj=new Scanner(System.in);
-		System.out.println("Enter number 1:");
-		int n1=obj.nextInt();
-		System.out.println("Enter number 2:");
-		int n2=obj.nextInt();
-		System.out.println("Enter number 3:");
-		int n3=obj.nextInt();
-		if(n1==n2 && n2==n3)
-			System.out.println("All same numbers");
+  public static void main(String arg[])
+   {	
+	Scanner obj= new Scanner(System.in);
+	System.out.println("Enter 1st number :");
+	int n1= obj.nextInt( );
+	System.out.println("Enter 2nd number :");
+	int n2= obj.nextInt( );
+	System.out.println("Enter 3rd number :");
+	int n3= obj.nextInt( );
+	if(n1== n2 && n2== n3)  System.out.println("Equivalent numbers");
+	else
+	{
+		if(n1>n2)
+		{
+		   if(n1>n3) System.out.println(n1+" is greatest");
+		   else  System.out.println(n3+" is greatest");
+		}
 		else
 		{
-			if(n1>n2)
-			{
-				if(n1>n3)
-					System.out.println(n1+" is greater");
-				else
-					System.out.println(n3+" is greater");
-			}
-			else
-			{
-				if(n2>n3)
-					System.out.println(n2+" is greater");
-				else
-					System.out.println(n3+" is greater");	
-			}
+			if(n2>n3)  System.out.println(n2+" is greatest");
+			else  System.out.println(n3+" is greatest");	
 		}
 	}
+   }
 }
